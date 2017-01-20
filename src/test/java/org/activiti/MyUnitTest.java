e42cb847730d6a26d43fc61e53c6ce4c1cc44665
Mar Gro <gromar01@gmail.com>
2017-01-20 16:14:06 +0100

    documentation expressions

--- src/test/java/org/activiti/MyUnitTest.java
+++ src/test/java/org/activiti/MyUnitTest.java
@@ -6,21 +6,28 @@ import org.activiti.engine.test.Deployment;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class MyUnitTest {
-	
+
 	@Rule
 	public ActivitiRule activitiRule = new ActivitiRule();
-	
+
 	@Test
 	@Deployment(resources = {"org/activiti/test/my-process.bpmn20.xml"})
 	public void test() {
-		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process");
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("assignee", "newAssignee");
+		ProcessInstance processInstance = activitiRule.getRuntimeService().startProcessInstanceByKey("my-process",
+				variables);
 		assertNotNull(processInstance);
-		
+
 		Task task = activitiRule.getTaskService().createTaskQuery().singleResult();
-		assertEquals("Activiti is awesome!", task.getName());
+		assertEquals("doc for newAssignee", task.getDescription());
 	}
 
 }
